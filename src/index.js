6179b84fbb18950be4d2017c3c4bdeccb7c87c96
aeroman256 <zakharov.dmitry.92@yandex.ru>
2020-03-06 00:10:45 +0300

    add API class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//https://pokeapi.co/api/v2/pokemon/1231/
+//https://swapi.co/api/people/1/
+class SwapiService {
+    _apiBase = 'https://swapi.co/api/'
+    async getResourse (url) {
+        const res = await fetch(`${this._apiBase}${url}`);
 
-ReactDOM.render(<h1>Star DB</h1>, document.getElementById('root'));
-// https://swapi.co
+        if (!res.ok) {
+            throw new Error(` Could not fetch ${url}, received ${res.status}`)
+        }
+    return await res.json();
+    }
+
+    async getAllPeople() {
+        const res = await this.getResourse(`people/`)
+        return res.results
+    }
+
+    getPeopleId(id) {
+        return this.getResourse(`people/${id}/`)
+    }
+
+    async getAllPlanets() {
+        const res = await this.getResourse(`planets/`)
+        return res.results
+    }
+
+    getPlanetsId(id) {
+        return this.getResourse(`planets/${id}/`)
+    }
+
+    async getAllStarships() {
+        const res = await this.getResourse(`starships/`)
+        return res.results
+    }
+
+    getStarshipsId(id) {
+        return this.getResourse(`starships/${id}/`)
+    }
+
+}
+
+const swapi = new SwapiService()
+swapi.getAllPeople().then((people) => {
+    people.forEach(element => {
+        console.log(element.name)
+    });
+})
